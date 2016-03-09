7f18b4e41bc35bce299f70f09d2af86bfc2bf377
William Calderipe <wcalderipe@gmail.com>
2016-03-09 20:49:34 -0300

    Setup application gems

--- Gemfile
+++ Gemfile
@@ -1,9 +1,19 @@
 source 'https://rubygems.org'
 
+gem 'rake', '< 11.0'
 gem 'rails', '4.2.4'
+gem 'rails-api'
 gem 'pg'
+gem 'responders', '~> 2.0'
+gem 'active_model_serializers'
 
 group :development, :test do
   gem 'byebug'
+  gem 'rspec-rails', '~> 3.0'
+  gem 'ffaker'
+  gem 'factory_girl_rails', '~> 4.0'
+  gem 'shoulda-matchers', '~> 3.0'
+  gem 'annotate', '2.6.5'
+  gem 'database_cleaner'
+  gem 'simplecov', :require => false
 end
-
---
+++ spec/rails_helper.rb
@@ -0,0 +1,47 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+require 'spec_helper'
+require 'rspec/rails'
+require 'shoulda/matchers'
+require 'simplecov'
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+ActiveRecord::Migration.maintain_test_schema!
+
+SimpleCov.start do
+    add_filter '/spec/'
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+    with.library :action_controller
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = 'random'
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |e|
+    DatabaseCleaner.cleaning do
+      e.run
+    end
+  end
+end
